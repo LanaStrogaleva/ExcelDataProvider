e8790b9a22c15ce722404be0d7ee0362c3c502ef
Svetlana Strogaleva <lana.strogaleva@gmail.com>
2024-11-20 22:48:32 +0300

    ExcelReader

--- src/test/java/ExcelReader.java
+++ src/test/java/ExcelReader.java
@@ -1,7 +1,51 @@
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ExcelReader {
     /**
      * Конструктор для инициализации листа в эксель файле
      * @param excelFilePath путь к файлу
      * @throws IOException
      */
+    private final String excelFilePath;
+    private XSSFSheet sheet;
+    private XSSFWorkbook book;
+    public ExcelReader(String excelFilePath, String sheetName) throws IOException {
+        this.excelFilePath = excelFilePath;
+        File file = new File(excelFilePath);
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            book = new XSSFWorkbook(fileInputStream);
+            sheet = book.getSheet(sheetName);
+        } catch (IOException e) {
+            throw new IOException("Не поддерживаемый формат");
+        }
+    }
+    public String cellToString(XSSFCell cell) throws Exception {
+        Object result = null;
+        CellType type = cell.getCellType();
+        switch (type) {
+            case NUMERIC:
+                result = cell.getNumericCellValue();
+                break;
+            case STRING:
+                result = cell.getStringCellValue();
+                break;
+            case FORMULA:
+                result = cell.getCellFormula();
+                break;
+            case BLANK:
+                result = "";
+            default:
+                throw new Exception("Ошибка чтения ячейки");
+        }
+        return result.toString();
+
+        }
 }
